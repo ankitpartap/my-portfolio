9354312954448f3afa3a2861eb87cb518964caee
Ankit Partap <84763601+ankitpartap@users.noreply.github.com>
2024-06-06 22:19:14 +0530

    Changing the description

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Ankit</h1>
-            <p className={styles.description}>I am passionate about training young minds in technology and dedicated to helping businesses grow by providing innovative tech-based solutions.</p>
+            <p className={styles.description}>Passionate about learning new technologies, sharing my knowledge with others and helping businesses to grow by providing tech-based solutions. I'm open to job opportunities where I can contribute, learn, and grow.</p>
             <a href="https://wa.me/917888912043?text=Hi!%20I'm%20found%20your%20profile%20intersted.%20Would%20you%20please%20tell%20me%20more%20about%20youself?" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/picofme2.png")} className={styles.heroImg} alt="image of me" />
