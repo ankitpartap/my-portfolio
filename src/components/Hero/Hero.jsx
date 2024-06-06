3c5ade011c27c4b5c5327c367baed43ea2625f32
Ankit Partap <84763601+ankitpartap@users.noreply.github.com>
2024-06-06 12:46:25 +0530

    Changes in whatsapp link text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Ankit</h1>
             <p className={styles.description}>I am passionate about training young minds in technology and dedicated to helping businesses grow by providing innovative tech-based solutions.</p>
-            <a href="https://wa.me/917888912043?text=Hi!%20I'm%20found%20your%20profile%20intersted.%20Would%20you%20please%20tell%20me%20more%20about%20you?" className={styles.contactBtn}>Contact me</a>
+            <a href="https://wa.me/917888912043?text=Hi!%20I'm%20found%20your%20profile%20intersted.%20Would%20you%20please%20tell%20me%20more%20about%20youself?" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/picofme2.png")} className={styles.heroImg} alt="image of me" />
         <div className={styles.topBlur}></div>
