da0df563fdc76621dbef8837a97fecd552531524
ankitpartap <ankitpartap24@gmail.com>
2025-08-22 10:24:03 +0530

    link edited

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -16,7 +16,7 @@ export const ProjectCard = ({project:{title, imageSrc, description, skills, demo
         </ul>
         
         <div className={styles.links}>
-            {demo && <a href={demo} className={styles.link}>Demo</a>}
+            {demo && <a href={demo} className={styles.link} target='new'>Demo</a>}
             {/* <a href={source} className={styles.link}>Source</a> */}
         </div>
     </div>
